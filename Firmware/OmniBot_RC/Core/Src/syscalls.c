b6cae75a6e76543257424b8dea879462af5a2941
Gabriel Lima <gabrielmartinslima@gmail.com>
2025-06-22 16:55:48 -0300

    Firmware: Log transmitted to UART2

--- Firmware/OmniBot_RC/Core/Src/syscalls.c
+++ Firmware/OmniBot_RC/Core/Src/syscalls.c
@@ -35,6 +35,7 @@
 extern int __io_putchar(int ch) __attribute__((weak));
 extern int __io_getchar(void) __attribute__((weak));
 
+extern UART_HandleTypeDef huart2;
 
 char *__env[1] = { 0 };
 char **environ = __env;
@@ -80,12 +81,9 @@ __attribute__((weak)) int _read(int file, char *ptr, int len)
 __attribute__((weak)) int _write(int file, char *ptr, int len)
 {
   (void)file;
-  int DataIdx;
 
-  for (DataIdx = 0; DataIdx < len; DataIdx++)
-  {
-    __io_putchar(*ptr++);
-  }
+  HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, 10);
+
   return len;
 }
 
