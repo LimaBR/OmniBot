d13798ef8d189bd74a579c0eedd967aa9a147811
Gabriel Lima <gabrielmartinslima@gmail.com>
2025-07-07 22:23:54 -0300

    Firmware: Add RobotCommandPacket.hpp

---
+++ Firmware/OmniBot_RC/Application/RobotCommandPacket.hpp
@@ -0,0 +1,18 @@
+/*
+ * RobotCommandPacket.hpp
+ *
+ *  Created on: Jul 6, 2025
+ *      Author: Gabriel
+ */
+
+#ifndef ROBOTCOMMANDPACKET_HPP_
+#define ROBOTCOMMANDPACKET_HPP_
+
+class RobotCommandPacket {
+public:
+	float tangentSpeed;
+	float normalSpeed;
+	float angularSpeed;
+};
+
+#endif /* ROBOTCOMMANDPACKET_HPP_ */
